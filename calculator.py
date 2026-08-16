760e56b39f066b4fd097950afe488cf96e9e9020
Pragati Sanjay Kharat <pragatikharat535@gmail.com>
2026-08-16 20:17:09 +0530

    Add simple calculator functionality in calculator.py
    Added Calculator Mini Project

---
+++ calculator.py
@@ -0,0 +1,22 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Choose operation (1-4): ")
+
+if choice == "1":
+    print("Result =", num1 + num2)
+elif choice == "2":
+    print("Result =", num1 - num2)
+elif choice == "3":
+    print("Result =", num1 * num2)
+elif choice == "4":
+    print("Result =", num1 / num2)
+else:
+    print("Invalid Choice")
